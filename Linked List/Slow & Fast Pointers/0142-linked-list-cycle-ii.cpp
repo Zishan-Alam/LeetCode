bc7546d8ea81fee59434daa6b832ce1c2730febe
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-20 03:00:09 +0530

    Update 0142-linked-list-cycle-ii.cpp

--- Linked List/Slow & Fast Pointers/0142-linked-list-cycle-ii.cpp
+++ Linked List/Slow & Fast Pointers/0142-linked-list-cycle-ii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;  // Slow pointer ko head se initialize karte hain
-        ListNode* fast = head;  // Fast pointer bhi head se initialize hota hai
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        bool flag = false;  // Flag to check if cycle exists
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow){
+                fast = head;
 
-        // Fast aur slow pointers ko move karte hain jab tak cycle detect na ho
-        while(fast != nullptr && fast->next != nullptr) {
-            fast = fast->next->next;  // Fast pointer ko do steps aage badhao
-            slow = slow->next;        // Slow pointer ko ek step aage badhao
-
-            // Agar slow aur fast pointers milte hain, toh cycle hai
-            if(slow == fast) {
-                slow = head;  // Slow pointer ko head pe wapas le aao
-                flag = true;  // Cycle detect ho gayi, flag ko true set karo
-                break;        // Break out of the loop as cycle is detected
+                while(fast != slow){
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return slow;
             }
         }
-
-        // Agar cycle detect hui hai toh cycle ka starting point find karte hain
-        if(flag == true) {
-            // Jab tak slow aur fast pointers milte nahi, unko ek step aage badhate raho
-            while(slow != fast) {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return slow;  // Yahi wo node hai jahan cycle start hoti hai
-        }
-        else {
-            // Agar cycle nahi hai toh nullptr return karo
-            return nullptr;
-        }
+        return nullptr;;
     }
 };
