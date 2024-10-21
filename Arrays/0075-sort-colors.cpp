f5d6a1e2cc9405b4b7c0038ff1fe2d30c16b6635
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-21 22:10:59 +0530

    Create 0075-sort-colors.cpp

---
+++ Arrays/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        //counting the occurences of 0s, 1s, 2s
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0) count0++;
+            else if(nums[i] == 1) count1++;
+            else count2++;
+        }
+        //replacing the values of each
+        for(int i=0; i<nums.size(); i++){
+            if(i<count0) nums[i] = 0;
+            else if(i<(count0+count1)) nums[i] = 1;
+            else nums[i] = 2;
+        }
+        return;
+    }
+};
