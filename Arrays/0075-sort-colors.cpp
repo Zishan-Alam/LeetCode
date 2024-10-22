0099ccb0f376cf9427abe5996d8d6e4197b29e7b
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-22 17:27:46 +0530

    Update 0075-sort-colors.cpp

--- Arrays/0075-sort-colors.cpp
+++ Arrays/0075-sort-colors.cpp
@@ -20,3 +20,30 @@ public:
         return;
     }
 };
+
+//.........................................................DUTCH NATIONAL FLAG ALGORITHM............................................................................//
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size()-1;
+
+        while(mid<=high){
+            if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else{
+                mid++;
+            }
+        }
+        return;
+    }
+};
