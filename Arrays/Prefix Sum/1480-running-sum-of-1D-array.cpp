e627580ce4fc1632c3ee238f23f6d07d6f72c74b
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-23 16:42:03 +0530

    Update 1480-running-sum-of-1D-array.cpp

--- Arrays/Prefix Sum/1480-running-sum-of-1D-array.cpp
+++ Arrays/Prefix Sum/1480-running-sum-of-1D-array.cpp
@@ -4,10 +4,7 @@ public:
         int n = nums.size();
 
         // Start from the second element and accumulate the sum
-        for (int i = 1; i < n; i++) {
-            nums[i] = nums[i - 1] +
-                      nums[i]; // Add the previous element to the current one
-        }
+        for (int i = 1; i < n; i++) nums[i] = nums[i] + nums[i-1]; // Add the previous element to the current one
 
         return nums; // Return the modified nums vector
     }
