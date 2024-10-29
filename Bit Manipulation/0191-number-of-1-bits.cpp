2cb85bab81618cc3b34bcb4a8e96b09cb1afa4df
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-29 17:25:09 +0530

    Create 0191-number-of-1-bits.cpp

---
+++ Bit Manipulation/0191-number-of-1-bits.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> result(n+1);
+
+        if(n == 0) return result;
+
+        result[0] = 0;
+
+        for(int i=0; i<result.size(); i++) {
+
+            if( i % 2 == 0) result[i] = result[i/2];
+            else result[i] = result[i/2] + 1;
+
+        }
+        return result;
+    }
+};
