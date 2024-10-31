58e2e2399469f075a68e22ad6ae7889669e0ea64
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-31 21:25:10 +0530

    Create 0215-kth-largest-element-in-an-array.cpp

---
+++ Heap/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,27 @@
+// Sorting approach
+// TC : O(nlogn)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return nums[n-k];
+    }
+};
+
+// Heap Approach
+//TC : O(n)
+//SC : O(k)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(auto &it : nums) {
+            pq.push(it);
+
+            if(pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
