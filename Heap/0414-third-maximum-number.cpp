ef0efcdcc36fd548b30b503114b684badcfa4f76
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-31 21:51:23 +0530

    Create 0414-third-maximum-number.cpp

---
+++ Heap/0414-third-maximum-number.cpp
@@ -0,0 +1,40 @@
+//Sorting Approach
+// TC : O(nlogn)
+// SC : O(1)
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+
+        auto last = unique(nums.begin(), nums.end());
+        nums.erase(last, nums.end());
+
+        int n = nums.size();
+
+        return (n >= 3)? nums[n-3] : nums.back();
+    }
+};
+
+//- Time complexity: O(n log n)
+//- Space complexity: O(n) 
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> st;
+        for(auto &it : nums) st.insert(it);                    //O(nlogn) time for insertion in set, space is o(n) for n distinct elements
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(auto &it : st) {
+            pq.push(it);
+
+            if(pq.size() > 3) pq.pop();                        //O(3) at most 3 elements can be inserted in heap, hence space is O(1) for heap
+        }
+
+        if(pq.size() < 3) {
+            while( pq.size() > 1 ) pq.pop();                  //this will also run at maximum of 2 times
+        }
+        return pq.top();
+    }
+};
