2e118f6bf619a17dfef104562272c7fb1a0f57a1
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-10-31 16:48:26 +0530

    Create 1962-remove-stones-to-minimize-the-total.cpp

---
+++ Heap/1962-remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        int n = piles.size();
+        priority_queue<int> pq;     //default max heap
+
+        int sum = 0;
+        
+        // Pushing elements into the heap and calculating sum   O(n)
+        for(auto it : piles) { 
+            pq.push(it);
+            sum += it;
+        }
+
+        // Removing the maximum elements    O(k * logn)
+        while( k != 0) { 
+            int maxi = pq.top();
+            pq.pop();
+
+            int remove = maxi/2;
+
+            sum -= remove;
+
+            maxi -= remove;
+
+            pq.push(maxi); //O(logn) to rearrange the elements of heap
+
+            k--;
+        }
+        return sum;
+    }
+};
