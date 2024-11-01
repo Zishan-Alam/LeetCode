f34108501b340dc5982ea2de379fecaeda63ad28
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-11-01 19:20:24 +0530

    Create 0373-find-k-pairs-with-smallest-sums.cpp

---
+++ Heap/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,42 @@
+// Time complexity is `O(m * n * log k)` 
+// Space complexity is `O(k)`.
+class Solution {
+public:
+    typedef pair<int, pair<int,int>> P; // P ko ek type define kar rahe hain jo sum aur index pair ko store karega
+
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        priority_queue<P> pq; // max-heap (priority queue) ka use kar rahe hain
+        vector<vector<int>> res; // result store karne ke liye
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int sum = nums1[i] + nums2[j]; // nums1 aur nums2 ke elements ka sum
+
+                if(pq.size() < k) {
+                    pq.push({sum, {i, j}}); // agar queue ka size k se chhota hai toh sum ko push karenge
+                }
+                else if(pq.top().first > sum) { // agar current sum priority queue ke top se chhota hai toh
+                    pq.pop(); // top ko pop karenge
+                    pq.push({sum, {i, j}}); // naye sum ko push karenge
+                }
+                else {
+                    break; // agar queue mein sum bada hai toh break karna hai kyunki sorted order mein hain
+                }
+            }
+        }
+
+        while(!pq.empty()) {
+            P temp = pq.top();
+            pq.pop();
+
+            int i = temp.second.first; // first index
+            int j = temp.second.second; // second index
+
+            res.push_back({nums1[i], nums2[j]}); // result mein pair ko add karna
+        }
+        return res; // final result return karna
+    }
+};
