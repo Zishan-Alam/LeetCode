9c5765a146dda0946112331dce2fff0a53a02673
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-11-01 19:57:48 +0530

    Update 0373-find-k-pairs-with-smallest-sums.cpp

--- Heap/0373-find-k-pairs-with-smallest-sums.cpp
+++ Heap/0373-find-k-pairs-with-smallest-sums.cpp
@@ -40,3 +40,49 @@ public:
         return res; // final result return karna
     }
 };
+
+// Time complexity is O(k log k) 
+// Space complexity is O(k).
+class Solution {
+public:
+
+    typedef pair<int, pair<int, int>> P;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        priority_queue<P, vector<P>, greater<P>> pq; // Min-heap
+        set<pair<int, int>> visited; // Set to track visited pairs
+        vector<vector<int>> result;
+
+        // Initial pair {0,0} ko push karna
+        int sum = nums1[0] + nums2[0];
+        pq.push({sum, {0, 0}});
+        visited.insert({0, 0});
+
+        while (k-- && !pq.empty()) {
+            auto temp = pq.top();
+            pq.pop();
+
+            int i = temp.second.first;
+            int j = temp.second.second;
+
+            result.push_back({nums1[i], nums2[j]});
+
+            // Push {i, j+1} if possible
+            if (j < n - 1 && visited.find({i, j + 1}) == visited.end()) {
+                int sum = nums1[i] + nums2[j + 1];
+                pq.push({sum, {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
+            // Push {i+1, j} if possible
+            if (i < m - 1 && visited.find({i + 1, j}) == visited.end()) {
+                int sum = nums1[i + 1] + nums2[j];
+                pq.push({sum, {i + 1, j}});
+                visited.insert({i + 1, j});
+            }
+        }
+        return result;
+    }
+};
+
