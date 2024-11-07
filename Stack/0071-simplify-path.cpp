45e3a77d8ff72a31dd313c029fd30c23329045b2
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-11-07 22:28:12 +0530

    Update 0071-simplify-path.cpp

--- Stack/0071-simplify-path.cpp
+++ Stack/0071-simplify-path.cpp
@@ -1,3 +1,4 @@
+// Using stack
 // time complexity of the `simplifyPath` function is O(N), where N is the length of the input string `path`. 
 // space complexity is also O(N) in the worst case.
 class Solution {
@@ -21,6 +22,32 @@ public:
         while(!st.empty()) {
             result = "/" + st.top() + result;       //reversing the result by adding result at last
             st.pop();
+        }
+        return result;
+    }
+};
+// Using string
+// time complexity of the `simplifyPath` function is O(n), where n is the length of the input string `path`
+// space complexity is O(m), where m is the number of valid directory names in the simplified path. 
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> vec;
+        string result = "";
+
+        stringstream ss(path);
+        string token = "";
+
+        while(getline(ss, token, '/')) {
+            if(token == "" || token == ".") continue;
+            if(token != "..") vec.push_back(token);
+            else if(!vec.empty()) vec.pop_back();
+        }
+
+        if(vec.size() == 0) return "/";
+
+        for(auto token : vec) {
+            result = result + "/" + token;
         }
         return result;
     }
