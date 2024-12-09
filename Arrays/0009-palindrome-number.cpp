d8c2ead60a1f9fffcbbbcb2c8b3eb941f3853948
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-12-09 12:32:19 +0530

    Create 0009-palindrome-number.cpp

---
+++ Arrays/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    long long int rev(int x) {
+        long long int rev = 0;
+        while(x != 0) {
+            int digit = x % 10;
+            rev = (rev * 10) + digit;
+            x /= 10;
+        }
+        return rev;
+    }
+
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+
+        long long int reverse = rev(x);
+
+        if(x == reverse) return true;
+        return false;
+    }
+};
