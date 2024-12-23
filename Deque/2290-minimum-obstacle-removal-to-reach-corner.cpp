a0676b0ce49a25fd2fb22dd8698947dce3f59757
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-12-23 11:41:14 +0530

    Update 2290-minimum-obstacle-removal-to-reach-corner.cpp

--- Deque/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ Deque/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,3 +1,5 @@
+// time complexity of the given solution is O(m * n), where m is the number of rows and n is the number of columns in the grid.
+// space complexity is also O(m * n) due to the distance matrix that stores the minimum number of obstacles encountered to reach each cell. Additionally, the deque used for BFS can also grow to O(m * n) in the worst case if all cells are added to it.
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
