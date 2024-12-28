5e99d60c0855e88d431407128e42beb21af351cb
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2024-12-28 14:41:09 +0530

    Update 0414-third-maximum-number.cpp

--- Heap/0414-third-maximum-number.cpp
+++ Heap/0414-third-maximum-number.cpp
@@ -7,6 +7,7 @@ public:
     int thirdMax(vector<int>& nums) {
         
         sort(nums.begin(), nums.end());
+        
 
         auto last = unique(nums.begin(), nums.end());
         nums.erase(last, nums.end());
