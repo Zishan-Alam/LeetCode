6f835b3b8f9e1fc476d91281785a004df4950e63
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-01-07 01:16:02 +0530

    Create 0046-permutations.cpp

---
+++ Backtracking/0046-permutations.cpp
@@ -0,0 +1,36 @@
+//- Time Complexity: O(n * n!)
+//- Space Complexity: O(n * n!)
+class Solution {
+public:
+
+    int n;
+    vector<vector<int>> result;
+    unordered_set<int> st;
+
+    void solve(vector<int> &temp, vector<int> &nums) {
+        if(temp.size() == n) {
+            result.push_back(temp);
+            return;
+        }
+
+        for(int i=0; i<n; i++) {
+
+            if(st.find(nums[i]) == st.end()) {
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+
+                solve(temp, nums);
+                
+                temp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        vector<int> temp;
+        solve(temp, nums);
+        return result;
+    }
+};
