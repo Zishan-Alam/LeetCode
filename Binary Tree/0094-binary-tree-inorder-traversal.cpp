828c23d94144875b8dcf337d8989aedfbe7b50fe
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-01-13 16:39:16 +0530

    Update 0094-binary-tree-inorder-traversal.cpp

--- Binary Tree/0094-binary-tree-inorder-traversal.cpp
+++ Binary Tree/0094-binary-tree-inorder-traversal.cpp
@@ -1,21 +1,65 @@
+//Recursive Approach
+class Solution {
+public:
+
+    void solve(TreeNode* root, vector<int> &res) {
+        if(!root) return;
+
+        solve(root->left, res);
+        res.push_back(root->val);
+        solve(root->right, res);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> res;
+
+        solve(root, res);
+        return res;
+    }
+};
+
+//Iterative Approach (2 Stacks)
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        //base case
+        if(!root) return {};
+
+        stack<TreeNode*> st;
+        stack<bool> visited;
+
+        st.push(root);
+        visited.push(0);
+
         vector<int> res;
-        if(root == NULL){
-            return res;
+
+        while(!st.empty()) {
+            TreeNode* temp = st.top();
+            st.pop();
+            bool flag = visited.top();
+            visited.pop();
+
+            //First Time Visit
+            if(flag == 0) {
+                //Right
+                if(temp->right) {
+                    st.push(temp->right);
+                    visited.push(0);
+                }
+                //Node
+                st.push(temp);
+                visited.push(1);
+                //Left
+                if(temp->left) {
+                    st.push(temp->left);
+                    visited.push(0);
+                }
+            }
+            //Second Time Visite
+            else{
+                res.push_back(temp->val);
+            }
         }
-
-        //LEFT
-        inorderTraversal(root->left);
-
-        //NODE
-        res.push_back(root->val);
-
-        //RIGHT
-        inorderTraversal(root->right);
-
         return res;
     }
 };
