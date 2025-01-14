468c791c9f0e63ac6ed2cc9c95a2fbb90002c23c
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-01-14 16:57:51 +0530

    Update 0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp

--- Binary Tree/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ Binary Tree/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -29,3 +29,37 @@ public:
         return solve(preorder, inorder, 0, preorder.size() - 1, idx);
     }
 };
+
+//TC : O(n);
+//SC : O(n)
+class Solution {
+public:
+
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int inStart, int inEnd, int &idx, unordered_map<int, int> &mp) {
+        if(inStart > inEnd) return nullptr;
+
+        int rootVal = preorder[idx];
+        int inIdx = mp[rootVal];
+
+        idx++;
+
+        TreeNode* root = new TreeNode(rootVal);
+
+        root->left  = solve(preorder, inorder, inStart, inIdx-1, idx, mp);
+        root->right = solve(preorder, inorder, inIdx+1, inEnd, idx, mp);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        int idx = 0;
+
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; i++) {
+            mp[inorder[i]] = i;
+        }
+
+        return solve(preorder, inorder, 0, n-1, idx, mp);
+    }
+};
