d4405a0909dae2a5c8f8bd13f8ae9006be9224d3
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-01-17 21:59:47 +0530

    Update 0283-move-zeroes.cpp

--- Arrays/Two Pointers/0283-move-zeroes.cpp
+++ Arrays/Two Pointers/0283-move-zeroes.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0; // `left` pointer jo non-zero elements ko track karega aur kaha place karna hai, wo decide karega
-        
-        // Right pointer loop ke saath array ke sabhi elements par chalega
-        for(int right = 0; right < nums.size(); right++) {
-            if(nums[right] != 0) { // Agar current element zero nahi hai
-                swap(nums[right], nums[left]);
-                left++; // `left` pointer ko ek position aage badhate hain taaki agle non-zero element ke liye space mile
+        int i = 0;
+        int j = 0;
+
+        //0 to j-1 --> non-zero
+        //j to i-1 --> zero
+        //i to end --> unknown
+
+        while(i < nums.size()) {
+            if(nums[i] == 0) i++;
+            else{
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
             }
         }
     }
