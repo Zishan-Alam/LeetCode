8da903ef12968c9f2c5482934ed5548c913d0a17
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-01-19 13:57:08 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- Arrays/Two Pointers/0026-remove-duplicates-from-sorted-array.cpp
+++ Arrays/Two Pointers/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
         int n = nums.size();
 
-        int k=0; //iterator for unique index
-        int i=0; //iterator to compare num[i] and num[j]
-        int j=1; //iterator to check unique elements
+        int i=0;     //always point to the unique element
+        int j=1;     //will iterate through the array
 
-        while(j<n){
-            if(nums[i] == nums[j]){
-                j++; //elements are same
+        while(j < n) {
+            if(nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
             }
-            else if(nums[i] != nums[j]){
-                nums[k+1] = nums[j]; //next unique index mai put karo
-                i=j; 
-                j++;
-                k++;
-            }
+            j++;
         }
-        return k+1;
+        return i+1;    //total number of unique elements
     }
 };
