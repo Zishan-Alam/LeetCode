f5f38d049804a98174ffdb81b4ad9192b0fbf9a2
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-02-19 16:20:02 +0530

    Update 0007-reverse-integer.cpp

--- Arrays/0007-reverse-integer.cpp
+++ Arrays/0007-reverse-integer.cpp
@@ -4,7 +4,6 @@ public:
         long long int rev = 0;
         while(x!=0){
             int lastDigit = x % 10;
-
             if(rev > INT_MAX/10 || rev < INT_MIN/10) return 0;
             rev = (rev * 10) + lastDigit;
             x = x / 10;
